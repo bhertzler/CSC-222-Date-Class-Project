0717a8b4bd5bf311766d75d84f70c06727ce33fd
Ben Hertzler <bwhertzler@outlook.com>
2024-12-13 04:07:38 -0500

    initial commit

--- Date.cpp
+++ Date.cpp
@@ -9,71 +9,92 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <exception>
 using namespace std;
 
 // Global Constants
-const string MONTH_NAMES[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-const int MONTH_LENGTHS[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+static const string MONTH_NAMES[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+static const int MONTH_LENGTHS[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+// Public Member Functions
 
 void Date::setDate(int mm, int dd, int yyyy)
 {
-	if (yyyy > 0 && yyyy < 10000)
-		year = yyyy;
-	else
-		year = 1930;
+	try
+	{
+		if (yyyy > 0 && yyyy < 10000)
+			year = yyyy;
+		else
+		{
+			throw range_error("Year Value Out of Range, defaulting to 1930.");
+			year = 1930;
+		}
 
-	setMonths;
+		setMonths;
 
-	if (mm > 0 && mm < 12)
-		month = mm;
-	else
-		month = 1;
+		if (mm > 0 && mm < 12)
+			month = mm;
+		else
+		{
+			throw range_error("Month Value Out of Range, defaulting to 1.");
+			month = 1;
+		}
 
-	if (dd > 0 && dd <= monthLength(month))
-		day = dd;
-	else
-		day = 1;
+		if (dd > 0 && dd <= monthLength(month))
+			day = dd;
+		else
+		{
+			throw range_error("Day Value Out of Range, defaulting to 1.");
+			day = 1;
+		}
+	}
+	catch (exception err)
+	{
+		cerr << "ERROR: " << err.what() << endl;
+	}
 }
 
-void Date::getDate(int& m, int& d, int& y)
+void Date::getDate(int& m, int& d, int& y) const
 {
 	m = month;
 	d = day;
 	y = year;
 }
 
-bool Date::isLeapYear(int y)
+bool Date::isLeapYear(int y) const
 {
-	if (y % 4 == 0 && y % 100 == 0)
-		return true;
-	else
-		return false;
+	return (y % 4 == 0 && y % 100 != 0 || y % 400 == 0);
 }
 
-int Date::monthLength(int m)
+int Date::monthLength(int m) const
 {
 	return months[m - 1].length;
 }
 
-string Date::monthName(int m)
+string Date::monthName(int m) const
 {
 	return months[m - 1].name;
 }
 
-void Date::printMDY()
+void Date::print(int format)
 {
-	cout << setprecision(2) << month << "/" << day << "/" << setprecision(4) << year << endl;
+	switch (format)
+	{
+	case 1:
+		cout << setprecision(2) << month << '/' << day << '/' << setprecision(4) << year << endl;
+		break;
+	case 2:
+		cout << monthName(month) << ' ' << setprecision(2) << day << ", " << setprecision(4) << year << endl;
+		break;
+	case 3:
+		cout << setprecision(2) << day << ' ' << monthName(month) << ' ' << setprecision(4) << year << endl;
+	default:
+		cerr << "INVALID OUTPUT FORMAT" << endl;
+		break;
+	}
 }
 
-void Date::printLongMDY()
-{
-	cout << monthName(month) << " " << setprecision(2) << day << ", " << setprecision(4) << year << endl;
-}
-
-void Date::printLongDMY()
-{
-	cout << setprecision(2) << day << " " << monthName(month) << " " << setprecision(4) << year << endl;
-}
+// Private Member Functions
 
 void Date::setMonths()
 {
@@ -87,7 +108,121 @@ void Date::setMonths()
 		months[1].length = 29;
 }
 
+int Date::julian() const
+{
+	int temp = (month - 14) / 12;
+	int jdn = (1461 * (year + 4800 + temp)) / 4;
+	jdn += (367 * (month - 2 - 12 * temp)) / 12;
+	jdn -= (3 * ((year + 4900 + temp) / 100)) / 4;
+	jdn += day;
+	jdn -= 32075;
+	return jdn;
+}
+
+// Operator Overloads
+
 Date Date::operator++()
 {
+	if (day == monthLength(month))
+	{
+		day = 1;
+		if (month == 12)
+		{
+			month = 1;
+			year++;
+			setMonths();
+		}
+		else
+			month++;
+	}
+	else
+		day++;
 
+	return *this;
+}
+
+Date Date::operator++(int)
+{
+	Date temp(month, day, year);
+
+	if (day == monthLength(month))
+	{
+		day = 1;
+		if (month == 12)
+		{
+			month = 1;
+			year++;
+			setMonths();
+		}
+		else
+			month++;
+	}
+	else
+		day++;
+
+	return temp;
+}
+
+Date Date::operator--()
+{
+	if (day == 1)
+	{
+		if (month = 1)
+		{
+			month = 12;
+			year--;
+			setMonths();
+		}
+		else
+			month--;
+		day = monthLength(month);
+	}
+	else
+		day--;
+
+	return *this;
+}
+
+Date Date::operator--(int)
+{
+	Date temp(month, day, year);
+
+	if (day == 1)
+	{
+		if (month = 1)
+		{
+			month = 12;
+			year--;
+			setMonths();
+		}
+		else
+			month--;
+		day = monthLength(month);
+	}
+	else
+		day--;
+
+	return temp;
+}
+
+int Date::operator-(const Date& opr) const
+{
+
+	Date temp(month, day, year);
+	return temp.julian() - opr.julian();
+
+}
+
+ostream& operator<<(ostream& strm, Date& dt)
+{
+	strm << dt.monthName(dt.month) << ' ' << setprecision(2) << dt.day << ", " << setprecision(4) << dt.year << " ";
+	return strm;
+}
+
+istream& operator>>(istream& strm, Date& dt)
+{
+	int temp_m, temp_d, temp_y;
+	strm >> temp_m >> temp_d >> temp_y;
+	dt.setDate(temp_m, temp_d, temp_y);
+	return strm;
 }
--- Date.h
+++ Date.h
@@ -10,13 +10,14 @@
 #include <iostream>
 using namespace std;
 
+// Class to manage a Date object including a month, day, and year.
 class Date
 {
 public:
 	// Mutators
 	
 	// Function to set the date.
-	// Postcondition: month = mm; day = dd; year = yyyy
+	// Postcondition:	month = mm; day = dd; year = yyyy
 	//		mm must be an int between 1 and 12;
 	//		dd must be an int between 1 and the length of the month;
 	//		yyyy must be a must be an int between 1 and 9999;
@@ -26,40 +27,53 @@ public:
 
 	// Function to return the date.
 	// Postconditon: m = month; d = day; y = year
-	void getDate(int& m, int& d, int& y);
+	void getDate(int& m, int& d, int& y) const;
 
 	// Function to check if year y is a leap year.
 	// Postcondition: Returns true if y is a leap year, otherwise false.
-	bool isLeapYear(int y);
+	bool isLeapYear(int y) const;
 
 	// Function to return the length of month m.
-	int monthLength(int m);
+	int monthLength(int m) const;
 
 	// Function to return the name of month m.
-	string monthName(int m);
+	string monthName(int m) const;
 
 	// Output
 
-	// Function to print the date numerically.
-	// Outputs in the format "MM/DD/YYYY"
-	void printMDY();
-
-	// Function to print the date descriptively.
-	// Outputs in the format "[MONTH NAME] DD, YYYY"
-	void printLongMDY();
-
-	// Function to print the date descriptively.
-	// Outputs in the format "DD [MONTH NAME] YYYY"
-	void printLongDMY();
-
+	// Function to output the date int format of the input parameter.
+	// Precondition: format must be an int between 1 and 3
+	// Postcondition:	Format 1: MM/DD/YYYY
+	//		Format 2: [MONTH NAME] DD, YYYY
+	//		Format 3: DD [MONTH NAME] YYYY
+	//		Other format values: Outputs error message
+	void print(int format);
 
 	// Operator Overloads
 
+	// Prefix ++ operator			
 	Date operator++();
+
+	// Postfix ++ operator
 	Date operator++(int);
+
+	// Prefix -- operator
 	Date operator--();
+
+	// Postfix -- operator
 	Date operator--(int);
+
+	// Overloaded subtraction operator (-)
+	// Postcondition:	returns the number of days between the dates subtracted.
+	int operator-(const Date&) const;
+
+	// Overloaded stream insertion operator (<<)
+	// Postcondition:	Outputs in the format "[MONTH] DD, YYYY ", does not end line by default.
 	friend ostream& operator<<(ostream&, Date&);
+
+	// Overloaded stream extraction operator (>>)
+	// Precondition:	Date must be entered as 3 whitespace separated integers in the format m d y.
+	// Postcondition:	cin >> date.month >> date.day >> date.year
 	friend istream& operator>>(istream&, Date&);
 
 	// Constructors
@@ -69,7 +83,10 @@ public:
 	Date() : month(1), day(1), year(1930) { setMonths; }
 
 	// Overloaded Constructor.
-	// Postcondition: month = mm; day = dd; year = yyyy;
+	// Postcondition:	month = mm; day = dd; year = yyyy;
+	//		mm must be an int between 1 and 12;
+	//		dd must be an int between 1 and the length of the month;
+	//		yyyy must be a must be an int between 1 and 9999;
 	Date(int mm, int dd, int yyyy) { setDate(mm, dd, yyyy); }
 
 private:
@@ -79,5 +96,6 @@ private:
 	struct monthType { string name; int length; };				// Struct to store month attributes
 	monthType months[12];										// Array to store the months
 	void setMonths();											// Private function to manage month attributes
+	int julian() const;											// Private function to return the Julian Day Number
 };
 
